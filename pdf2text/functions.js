e97fe1bae8f275f3242d27dadbb5a0e2c2245641
ROMIGUIERES William <wistaro@hotmail.fr>
2018-02-18 16:58:24 +0100

    Add loading/error message, add a page limit on the imported pdf

--- pdf2text/functions.js
+++ pdf2text/functions.js
@@ -24,10 +24,12 @@ function getPageText(pageNum, PDFDocumentInstance) {
 
 function handlePdf(data){
 
+    document.getElementById('loaderPdf').innerHTML = "<b><font color=#FE2E2E>Chargement du fichier en cours...</font></b>&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<img src='images/ajax-loader.gif' alt='loader' height='10px' width='10px'/>";
     document.getElementById('myText0').innerHTML = "* Début du fichier PDF*\n";
 
     var fileList = this.files;
     var MyFile = data[0];
+    var maxPages = 10;
     var fileReader = new FileReader();
 
     fileReader.onload = function() {
@@ -39,22 +41,30 @@ function handlePdf(data){
             var pdfDocument = pdf;
             var pagesPromises = [];
 
-        for (var i = 0; i < pdf.pdfInfo.numPages; i++) {
-            (function (pageNumber) {
-                pagesPromises.push(getPageText(pageNumber, pdfDocument));
-            })(i + 1);
-        }
+            if(pdf.pdfInfo.numPages <= maxPages){
 
-        Promise.all(pagesPromises).then(function (pagesText) {
-             document.getElementById('myText0').innerHTML += pagesText; //fill textarea with extracted text
+                for (var i = 0; i < pdf.pdfInfo.numPages; i++) {
+                    (function (pageNumber) {
+                        pagesPromises.push(getPageText(pageNumber, pdfDocument));
+                    })(i + 1);
+                }
+
+                Promise.all(pagesPromises).then(function (pagesText) {
+                     document.getElementById('myText0').innerHTML += pagesText; //fill textarea with extracted text
 
 
-        });
+                });
+                document.getElementById('loaderPdf').innerHTML = "<b><font color=#3ADF00>Fichier pdf chargé!</font></b>";
+            }else{
+                alert("Le document comporte trop de pages! Maxmimum "+maxPages+" pages.");
+                document.getElementById('loaderPdf').innerHTML = "<b><font color=#FE2E2E>Echec lors du chargement fichier pdf: trop de pages.</font></b>";
 
-        delete data;
+
+            }
 
         }, function (reason) {
-            alert(reason);
+            document.getElementById('loaderPdf').innerHTML = "<b><font color=#FE2E2E>"+reason+"</font></b>";
+
         });
 
     };
