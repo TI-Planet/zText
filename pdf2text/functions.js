440242115195364a901b9802dbd8451577b2b2d0
Adrien Bertrand <bertrand.adrien@gmail.com>
2018-02-21 00:38:15 +0800

    Code cleanup

--- pdf2text/functions.js
+++ pdf2text/functions.js
@@ -1,76 +1,74 @@
-function getPageText(pageNum, PDFDocumentInstance) {
-    return new Promise(function (resolve, reject) {
-        PDFDocumentInstance.getPage(pageNum).then(function (pdfPage) {
-            pdfPage.getTextContent().then(function (textContent) {
-                var textItems = textContent.items;
-                var finalString = "";
-                var lastY = -1;
-
-                 textContent.items.forEach(function (i) {
-                        if (lastY != i.transform[5]) {
-
-                          finalString += "\n";
-                          lastY = i.transform[5];
-                        }
-
-                  finalString += i.str;
+function getPageText(pageNum, PDFDocumentInstance)
+{
+    return new Promise(function(resolve, reject)
+    {
+        PDFDocumentInstance.getPage(pageNum).then(function(pdfPage)
+        {
+            pdfPage.getTextContent().then(function(textContent)
+            {
+                let finalString = "";
+                let lastY = -1;
+                textContent.items.forEach(function(i)
+                {
+                    if (lastY !== i.transform[5])
+                    {
+                        finalString += "\n";
+                        lastY = i.transform[5];
+                    }
+                    finalString += i.str;
                 });
-
                 resolve(finalString);
             });
         });
     });
 }
 
-function handlePdf(data){
+function handlePdf(files)
+{
+    if (typeof(files[0]) === "undefined") {
+        return;
+    }
 
-    document.getElementById('loaderPdf').innerHTML = "<b><font color=#FE2E2E>Chargement du fichier en cours...</font></b>&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<img src='images/ajax-loader.gif' alt='loader' height='10px' width='10px'/>";
+    document.getElementById('loaderPdf').innerHTML = "<b><font color=#FE2E2E>Chargement du fichier PDF en cours...</font></b>&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<img src='images/ajax-loader.gif' alt='loader' height='10px' width='10px'/>";
     document.getElementById('myText0').innerHTML = "* Début du fichier PDF*\n";
+
     $('#myText0').prop('disabled', true); //disable textarea while loading the pdf file
 
-    var fileList = this.files;
-    var MyFile = data[0];
-    var maxPages = 2000;
-    var fileReader = new FileReader();
+    const maxPages = 10;
+    const fileReader = new FileReader();
 
-    fileReader.onload = function() {
-
-        var typedarray = new Uint8Array(this.result);
-        
-         PDFJS.getDocument(typedarray).then(function(pdf) {
-
-            var pdfDocument = pdf;
-            var pagesPromises = [];
-
-            if(pdf.pdfInfo.numPages <= maxPages){
-
-                for (var i = 0; i < pdf.pdfInfo.numPages; i++) {
-                    (function (pageNumber) {
-                        pagesPromises.push(getPageText(pageNumber, pdfDocument));
+    fileReader.onload = function()
+    {
+        let typedarray = new Uint8Array(this.result);
+        PDFJS.getDocument(typedarray).then(function(pdfDoc)
+        {
+            if (pdfDoc.pdfInfo.numPages <= maxPages)
+            {
+                let pagesPromises = [];
+                for (var i = 0; i < pdfDoc.pdfInfo.numPages; i++)
+                {
+                    (function(pageNumber) {
+                        pagesPromises.push(getPageText(pageNumber, pdfDoc));
                     })(i + 1);
                 }
-
-                Promise.all(pagesPromises).then(function (pagesText) {
-                     document.getElementById('myText0').innerHTML += pagesText; //fill textarea with extracted text
-
-
+                Promise.all(pagesPromises).then(function(pagesText) {
+                    document.getElementById('myText0').innerHTML += pagesText; //fill textarea with extracted text
                 });
-                document.getElementById('loaderPdf').innerHTML = "<b><font color=#3ADF00>Fichier pdf chargé!</font></b>";
-                $('#myText0').prop('disabled', false); //enable the textarea
-            }else{
-                alert("Le document comporte trop de pages! Maxmimum "+maxPages+" pages.");
-                document.getElementById('loaderPdf').innerHTML = "<b><font color=#FE2E2E>Echec lors du chargement fichier pdf: trop de pages.</font></b>";
-                $('#myText0').prop('disabled', false); //enable the textarea
-
+                document.getElementById('loaderPdf').innerHTML = `<b><font color="#3ADF00">Fichier PDF chargé!</font></b>`;
+            }
+            else
+            {
+                alert(`Le document comporte trop de pages! Maxmimum ${maxPages} pages.`);
+                document.getElementById('loaderPdf').innerHTML = `<b><font color="#FE2E2E">Échec lors du chargement du PDF : trop de pages.</font></b>`;
             }
 
-        }, function (reason) {
-            document.getElementById('loaderPdf').innerHTML = "<b><font color=#FE2E2E>"+reason+"</font></b>";
+            $('#myText0').prop('disabled', false); //enable the textarea
+
+        }, function(reason) {
+            document.getElementById('loaderPdf').innerHTML = `<b><font color="#FE2E2E">${reason}</font></b>`;
             $('#myText0').prop('disabled', false); //enable the textarea
         });
-
-         
     };
 
-    fileReader.readAsArrayBuffer(MyFile);
+    fileReader.readAsArrayBuffer(files[0]);
 }
